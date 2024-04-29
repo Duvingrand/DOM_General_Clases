f0cd54d4c75ae46636775a6dac9e11dbd6f96a99
Duvingrand <Duvingrand@gmail.com>
2024-04-29 12:56:35 -0500

    otro

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,27 @@
-let one=document.getElementById("botonPrimary")
-let two
-let three
-let four
+console.groupCollapsed("getElementBy");
 
-console.log(one)
+let one = document.getElementById("botonPrimary");
+let two = document.getElementsByClassName("btn");
+let three = document.getElementsByName("three");
+let four = document.getElementsByTagName("button");
+
+console.log(one);
+console.log(two[1]);
+console.log(three[0]);
+console.log(four[3]);
+
+console.groupEnd();
+
+console.group("querySelector");
+
+let five = document.querySelector("#botonWarning")
+let six = document.querySelector(".btn")
+let seven = document.querySelector("[name='boton']")
+let eight=document.querySelector("button")
+let nine=document.querySelectorAll("button")
+
+console.log(five)
+console.log(six)
+console.log(seven)
+console.log(eight)
+console.log(nine)
